b74bb6b19023d97c835ac220fa2016e4784e22fd
Oleggood <oleggood@mail.ru>
2020-12-07 10:24:51 +0300

    не работает структура дочерних страниц, не правильно сделана машрутизация

---
+++ public_html/app/Http/Controllers/Front/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+//to do
+//скорее всего проблема в путях
+//class MainController extends Controller
+//{
+//    public function home() {
+//        return view('front.home');
+//    }
+//
+//    public function yes() {
+//        return view('/front/divination/yes');
+//    }
+//
+//}
